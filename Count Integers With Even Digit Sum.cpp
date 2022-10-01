181f2e3bb894713009ab73e925dc534038ea8c2b
DipanshuTyagi <85155638+DipanshuTyagi@users.noreply.github.com>
2022-10-01 12:45:28 +0530

    Create Count Integers With Even Digit Sum.cpp

---
+++ Count Integers With Even Digit Sum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countEven(int num) {
+     int sum = 0;
+       int n= num;
+    while (n) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return (sum & 1) ? (num - 1) / 2: num / 2;
+    }
+};
